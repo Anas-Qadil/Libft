f1723fe73d7fa373b7ac9e2ea09ea07b4e5c4f5a
Anas Qadil <aqadil@e2r9p1.1337.ma>
2021-11-02 18:45:54 +0100

    making some changes

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: aqadil <aqadil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:17:53 by aqadil            #+#    #+#             */
-/*   Updated: 2021/11/02 17:18:32 by aqadil           ###   ########.fr       */
+/*   Updated: 2021/11/02 18:45:12 by aqadil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,8 @@
 
 void				*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	mc;
-	unsigned char	*ms;
-
-	i = 0;
-	mc = (unsigned char)c;
-	ms = (unsigned char *)s;
-	while (i < n)
-	{
-		if (ms[i] == mc)
-			return (&ms[i]);
-		i++;
-	}
+	while (n--)
+		if (*(unsigned char *)s++ == (unsigned char )c)
+			return ((void *)--s);
 	return (NULL);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aqadil <aqadil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:21:14 by aqadil            #+#    #+#             */
-/*   Updated: 2021/11/02 17:21:22 by aqadil           ###   ########.fr       */
+/*   Updated: 2021/11/02 18:27:31 by aqadil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,8 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*ms1;
-	unsigned char	*ms2;
-
-	i = 0;
-	ms1 = (unsigned char *)s1;
-	ms2 = (unsigned char *)s2;
-	while (i < n && (ms1[i] || ms2[i]))
-	{
-		if (ms1[i] != ms2[i])
-			return (ms1[i] - ms2[i]);
-		i++;
-	}
+	while (n-- && (*s1 || *s2))
+		if (*(unsigned char *)s1++ != *(unsigned char *)s2++)
+			return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
 	return (0);
 }
